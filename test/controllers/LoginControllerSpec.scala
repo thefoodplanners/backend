e929c6d42b84dbe75131f2a2649c8b32a53b15f4
pavathon <markbrowne18@gmail.com>
2023-03-08 16:04:38 +0000

    Make changes to LoginControllerSpec for the test case to pass.

--- test/controllers/LoginControllerSpec.scala
+++ test/controllers/LoginControllerSpec.scala
@@ -7,7 +7,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 import org.scalatestplus.mockito.MockitoSugar.mock
-import play.api.http.Status.BAD_REQUEST
+import play.api.http.Status.{ BAD_REQUEST, OK }
 import play.api.libs.json.Json
 import play.api.test.Helpers.{ POST, contentAsString, defaultAwaitTimeout, status }
 import play.api.test.{ FakeRequest, Helpers }
@@ -31,20 +31,19 @@ class LoginControllerSpec extends AnyFlatSpec with Matchers {
 
     val jsonBody = Json.parse(
       """{
-        |  "date": "email1",
+        |  "email": "email1",
         |  "username": "username1",
         |  "password": "password1"
         |}""".stripMargin
     )
 
     val request = FakeRequest(POST, "/addMealSlot")
-      .withSession("USERID" -> "1")
       .withHeaders("Content-type" -> "application/json")
       .withBody(jsonBody)
 
     val result = controller.register().apply(request)
 
-    status(result) shouldBe BAD_REQUEST
-    contentAsString(result) shouldBe "Error in processing Json data in request body."
+    status(result) shouldBe OK
+    contentAsString(result) shouldBe "User successfully added."
   }
 }
